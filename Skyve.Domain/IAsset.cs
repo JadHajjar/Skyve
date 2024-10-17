30a3888c54990e508b7e9056d0abd098a863bdde
JadHajjar <jad@brains-lb.com>
2024-10-17 16:13:04 +0300

    Update IAsset.cs

--- Skyve.Domain/IAsset.cs
+++ Skyve.Domain/IAsset.cs
@@ -1,8 +1,11 @@
 ﻿using Skyve.Domain.Enums;
 
+using System.Collections.Generic;
+
 namespace Skyve.Domain;
 public interface IAsset : ILocalPackageIdentity
 {
 	IPackage? Package { get; set; }
 	AssetType AssetType { get; }
+	string[] Tags { get; }
 }
